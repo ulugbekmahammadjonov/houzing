98eb5c2f78003673c3113a789396287bac650646
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-12-25 22:12:00 +0500

    costum hooks created

---
+++ src/hooks/useSearch.jsx
@@ -0,0 +1,7 @@
+import { useLocation } from "react-router-dom";
+
+export const useSearch = ()=>{
+    const {search} = useLocation()
+    return new URLSearchParams(search)
+}
+export default useSearch;
