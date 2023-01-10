a9678d63f312eeffb78caf9a81787eee09449fa4
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2023-01-10 21:45:46 +0500

    finished filter

---
+++ src/components/Category/style.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin: auto;
+  padding: 96px 30px;
+  max-width: 1260px;
+  width: 100%;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 32px;
+`;
+
+
+
+export { Container,  Content };
--- src/components/HouseCard/index.jsx
+++ src/components/HouseCard/index.jsx
@@ -21,7 +21,8 @@ const HouseCard = ({ data={} }) => {
         <div className="subTitle inline">
           {city}, {country}, {description}
         </div>
-        <div className="info">{address || "Quincy St, Brooklyn, NY, USA"}-{" "}{category?.name || "Category"}</div>
+        <div className="info">{address || "Quincy St, Brooklyn, NY, USA"}-{" "}
+        {category?.name || "Category"} {houseDetails?.room || 0}-rooms</div>
         <Details>
           <Details.Item>
             <Icons.Bed />
